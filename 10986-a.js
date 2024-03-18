f65bf9969efadc1c85148ba5a924222e0f1e57a4
LeeDaeGyu <ttap0704@naver.com>
2024-03-18 21:57:46 +0900

    누적합 학습 4일차

--- 10986-a.js
+++ 10986-a.js
@@ -7,16 +7,31 @@ const input = fs
 
 const [N, M] = input.shift().split(" ");
 
+// (p[i] - [j]) % M = 0
+// (p[i] % M) - (p[j] % M) = 0
+// p[i] % M = p[j] % M
+// p[i] = p[j]
+
 function main(n, m, arr) {
-  const prefix = [];
+  const prefix = [],
+    prefixCount = {};
   let ret = 0;
 
   for (let i = 0; i < n; i++) {
     if (i == 0) prefix[i] = +arr[i] % m;
     else prefix[i] = (prefix[i - 1] + +arr[i]) % m;
+    prefix[i] %= m;
+
+    if (prefix[i] === 0) ret++;
+
+    prefixCount[prefix[i]] = prefixCount[prefix[i]]
+      ? prefixCount[prefix[i]] + 1
+      : 1;
   }
 
-  console.log({ prefix });
+  Object.values(prefixCount).forEach((val) => {
+    ret += (val * (val - 1)) / 2;
+  });
 
   return ret;
 }
